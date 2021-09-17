f4ff1fd50c118cc4062018817e08ad7cbcdfb7c7
Naveenbasyal <72588078+naveenbasyal@users.noreply.github.com>
2021-09-18 00:55:14 +0530

    Basename
    N

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import "../src/style.css";
 ReactDOM.render(
   <>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </>,
